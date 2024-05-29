e43a3c1421e184c90352dda24de58395d7930618
kmzhang1 <kmzhang@scu.edu>
2024-05-29 14:09:03 -0700

    updated chemical info

--- src/components/ChemicalsInfo.jsx
+++ src/components/ChemicalsInfo.jsx
@@ -25,6 +25,7 @@ const ChemicalsInfo = () => {
                 >
                   <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                     <h5 className="h5 mb-5">{item.title}</h5>
+                    <p className="body-2 mb-6 text-n-3">{item.industry}</p>
                     <p className="body-2 mb-6 text-n-3">{item.text}</p>
                     <div className="flex items-center mt-auto">
                       <img
